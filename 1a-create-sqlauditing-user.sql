655a4f754e350ca5d3304e42d07c658c7a29889a
sqlkitty <hellosqlkitty+github@gmail.com>
2022-04-22 11:51:17 -0600

    Create 1a-create-sqlauditing-user.sql

---
+++ 1a-create-sqlauditing-user.sql
@@ -0,0 +1,6 @@
+/* 
+create login on the central server 
+this login will be used on your linked server connection on your audited servers 
+*/ 
+
+CREATE LOGIN sqlauditing WITH PASSWORD='yourstrongpassword'
